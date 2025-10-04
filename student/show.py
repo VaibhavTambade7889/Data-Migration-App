e6e3f75ec9d9f9430ea546684c79bb0e57f6d309
VaibhavTambade7889 <rajtambade1760@gmail.com>
2025-10-04 20:19:38 +0530

    save.py : save_to_csv load.py : load_from_csv show.py : display_student_information

---
+++ student/show.py
@@ -0,0 +1,8 @@
+
+
+# L = [[101, 'AAA', 60], [102, 'BBB', 70], [103, 'CCC', 80]]
+
+def display_student_information(list_of_list): # L is list of list
+    print("RNO \t NAME \t PER")
+    for record in list_of_list:
+        print(record[0] ,"\t",record[1], "\t",record[2])
